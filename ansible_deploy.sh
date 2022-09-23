2b25b7995b71d520a8c4ab97ee526b9c8cb73011
Peter Plak <peter_plak@dell.com>
2022-09-23 06:52:54 +0200

    Revised setup

--- ansible_deploy.sh
+++ ansible_deploy.sh
@@ -1,1 +1,12 @@
-ansible-playbook -i hosts playbook.yml -vvv
+#!/bin/bash
+
+# COMMENTS on additional cli arguments when executing ./ansible_deploy.sh
+# --limit=spine -> run only on group spine
+# -vvvvv        -> Run with max verbose output
+
+for arg in "$@"
+do
+    args=$args" $arg"
+done
+
+ansible-playbook -i hosts $args playbook.yml
